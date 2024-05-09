2f03edaaae5d3099c9c504badd8837cb219c3a2b
Hiro Eernisse <40651088+hiromasae@users.noreply.github.com>
2024-05-09 04:51:35 +0000

    Fixed bug where howtoplay couldn't read txt file

--- craps/howtoplay/HowToPlay.cpp
+++ craps/howtoplay/HowToPlay.cpp
@@ -11,10 +11,16 @@ void howToPlayDoc() {
     std::cout << "--------------------------------------------------------------------------------\n";
 
     file.open("crapsrules.txt");
+
+    if (!file.is_open()) {
+        std::cout << "Error! Could not open file crapsrules.txt.";
+    }
+
     while (!file.eof()) {
         getline(file, line);
         std::cout << line << std::endl;
     }
+
     file.close();
     
     std::cout << "\n'Q' to return to menu:\n";
