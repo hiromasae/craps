44df431c07a4ab99416a74f50599c9377c9b5599
Hiro Eernisse <40651088+hiromasae@users.noreply.github.com>
2024-05-11 06:08:16 +0000

    Final touches and created README

--- README.md
+++ README.md
@@ -10,5 +10,26 @@ history can each be viewed through the menu selection.
 
 ## Execution
 
-Running the program will run all of the cpp files within. Then follow instructions and input in the <br/> 
-terminal.
+Enter the following into the terminal to run the program: <br/>
+```
+./build.sh && /workspaces/craps/craps/main 
+```
+
+## Stages of Development
+
+### Completed:
+- Menu creation
+- Craps game mechanic
+- The game mechanic integrated with the player and bot
+- Betting integrated, completing the game loop
+- Create linked list template for transaction history and roll history
+- Implement transaction history using template and print to screen
+- Implement roll history using template and print to screen
+
+### Future Stages:
+- Game rules read from file then print to screen
+- Print transaction history and roll history to respective txt files after exiting game
+- Unit tests for each class
+- Error messages for wrong input
+- Change "Game" to be a base class and make "Craps" its subclass. 
+- Search algorithm to find largest gain and loss from transactions 
--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
-g++ -c /workspaces/craps/craps/craps/Craps.cpp -o /workspaces/craps/craps/Craps.o
-g++ -c /workspaces/craps/craps/diceroll/DiceRoll.cpp -o /workspaces/craps/craps/DiceRoll.o
-g++ -c /workspaces/craps/craps/game/Game.cpp -o /workspaces/craps/craps/Game.o
-g++ -c /workspaces/craps/craps/howtoplay/HowToPlay.cpp -o /workspaces/craps/craps/HowToPlay.o
-g++ -c /workspaces/craps/craps/menu/Menu.cpp -o /workspaces/craps/craps/Menu.o
-g++ -c /workspaces/craps/craps/player/Player.cpp -o /workspaces/craps/craps/Player.o
-g++ -c /workspaces/craps/craps/playerbalancehistory/PlayerBalanceHistory.cpp -o /workspaces/craps/craps/PlayerBalanceHistory.o
-g++ -c /workspaces/craps/craps/rollhistory/RollHistory.cpp -o /workspaces/craps/craps/RollHistory.o
-g++ -c /workspaces/craps/craps/main.cpp -o /workspaces/craps/craps/main.o
-g++ /workspaces/craps/craps/Craps.o /workspaces/craps/craps/DiceRoll.o /workspaces/craps/craps/Game.o /workspaces/craps/craps/HowToPlay.o /workspaces/craps/craps/Menu.o /workspaces/craps/craps/Player.o /workspaces/craps/craps/PlayerBalanceHistory.o /workspaces/craps/craps/RollHistory.o /workspaces/craps/craps/main.o -o /workspaces/craps/craps/main
-echo "craps game compiled."
+g++ -c /workspaces/craps/craps/craps/Craps.cpp -o /workspaces/craps/buildfiles/Craps.o
+g++ -c /workspaces/craps/craps/diceroll/DiceRoll.cpp -o /workspaces/craps/buildfiles/DiceRoll.o
+g++ -c /workspaces/craps/craps/game/Game.cpp -o /workspaces/craps/buildfiles/Game.o
+g++ -c /workspaces/craps/craps/howtoplay/HowToPlay.cpp -o /workspaces/craps/buildfiles/HowToPlay.o
+g++ -c /workspaces/craps/craps/menu/Menu.cpp -o /workspaces/craps/buildfiles/Menu.o
+g++ -c /workspaces/craps/craps/player/Player.cpp -o /workspaces/craps/buildfiles/Player.o
+g++ -c /workspaces/craps/craps/playerbalancehistory/PlayerBalanceHistory.cpp -o /workspaces/craps/buildfiles/PlayerBalanceHistory.o
+g++ -c /workspaces/craps/craps/rollhistory/RollHistory.cpp -o /workspaces/craps/buildfiles/RollHistory.o
+g++ -c /workspaces/craps/craps/main.cpp -o /workspaces/craps/buildfiles/main.o
+g++ /workspaces/craps/buildfiles/Craps.o /workspaces/craps/buildfiles/DiceRoll.o /workspaces/craps/buildfiles/Game.o /workspaces/craps/buildfiles/HowToPlay.o /workspaces/craps/buildfiles/Menu.o /workspaces/craps/buildfiles/Player.o /workspaces/craps/buildfiles/PlayerBalanceHistory.o /workspaces/craps/buildfiles/RollHistory.o /workspaces/craps/buildfiles/main.o -o /workspaces/craps/buildfiles/main
