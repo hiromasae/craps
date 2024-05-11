37e098c1471b9def8a1f187f2d0477a697f65938
Hiro Eernisse <40651088+hiromasae@users.noreply.github.com>
2024-05-11 04:34:46 +0000

    Build file implemented, still need to make some final touches

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# craps
+# Craps
+
+A simplified version of the craps game.
+
+## Description
+
+This game of craps give you a starting balance of $100 and allows you to play against a bot to <br />
+potentially win money (or lose money). The game rules, current balance, transaction history, and roll <br/>
+history can each be viewed through the menu selection.
+
+## Execution
+
+Running the program will run all of the cpp files within. Then follow instructions and input in the <br/> 
+terminal.
---
+++ build.sh
@@ -0,0 +1,11 @@
+g++ -c /workspaces/craps/craps/craps/Craps.cpp -o /workspaces/craps/craps/Craps.o
+g++ -c /workspaces/craps/craps/diceroll/DiceRoll.cpp -o /workspaces/craps/craps/DiceRoll.o
+g++ -c /workspaces/craps/craps/game/Game.cpp -o /workspaces/craps/craps/Game.o
+g++ -c /workspaces/craps/craps/howtoplay/HowToPlay.cpp -o /workspaces/craps/craps/HowToPlay.o
+g++ -c /workspaces/craps/craps/menu/Menu.cpp -o /workspaces/craps/craps/Menu.o
+g++ -c /workspaces/craps/craps/player/Player.cpp -o /workspaces/craps/craps/Player.o
+g++ -c /workspaces/craps/craps/playerbalancehistory/PlayerBalanceHistory.cpp -o /workspaces/craps/craps/PlayerBalanceHistory.o
+g++ -c /workspaces/craps/craps/rollhistory/RollHistory.cpp -o /workspaces/craps/craps/RollHistory.o
+g++ -c /workspaces/craps/craps/main.cpp -o /workspaces/craps/craps/main.o
+g++ /workspaces/craps/craps/Craps.o /workspaces/craps/craps/DiceRoll.o /workspaces/craps/craps/Game.o /workspaces/craps/craps/HowToPlay.o /workspaces/craps/craps/Menu.o /workspaces/craps/craps/Player.o /workspaces/craps/craps/PlayerBalanceHistory.o /workspaces/craps/craps/RollHistory.o /workspaces/craps/craps/main.o -o /workspaces/craps/craps/main
+echo "craps game compiled."
